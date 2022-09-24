e5b21b3a8976da7946f6d36e8e4abd20f63ce783
nahsooyeon <nahsooyeon@gmail.com>
2022-09-25 00:53:52 +0900

    ✨ 추가: 로그인 기능 구현

---
+++ components/Login.tsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useRef } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebaseConfig";
+
+const LoginForm = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <button
+        onClick={() => signInWithGoogle([""], { prompt: "select_account" })}
+        className="min-w-[250px] h-20 rounded max-w-[30vw]  bg-primary text-white"
+      >
+        SING WITH GOOGLE
+      </button>
+    </div>
+  );
+};
+
+export default LoginForm;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebaseConfig";
+import LoginForm from "../components/Login";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return <>Loading...</>;
+  }
+
+  if (!user) {
+    return <LoginForm />;
+  }
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
