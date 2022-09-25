a36c0d5f1b9e128f757fc2d5c4d1eea02e369820
nahsooyeon <nahsooyeon@gmail.com>
2022-09-25 12:57:34 +0900

    🚸 수정: 새 채팅방 추가시 이메일 주소 유효성 검증

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect, useCallback, memo } from "react";
 import { signOut, User } from "firebase/auth";
-import { auth, db } from "../lib/firebaseConfig";
+import { auth, db } from "../firebase.config";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 // import ChatListItem from "./ChatListItem";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -15,25 +15,34 @@ export interface IChat {
 
 import { useRouter } from "next/router";
 import ChatListItem from "./ChatListItem";
+import { isEmail } from "class-validator";
+import { isEmpty } from "lodash";
 
 const Sidebar = () => {
   const [user] = useAuthState(auth);
+  const router = useRouter();
   const [snapshot, loading, error] = useCollection(collection(db, "chats"));
 
   const isChatExists = (email: string) =>
     chats?.find(
       (chat) =>
-        chat.users.includes(user?.email as string) && chat.users.includes(email)
+        chat.users?.includes(user?.email as string) &&
+        chat.users.includes(email)
     );
 
   const newChat = async () => {
     const input = prompt("대화를 시작할 친구의 이메일 주소를 입력해주세요.");
-    if (
+    if (isEmpty(input)) {
+      return;
+    } else if (
       !isChatExists(input as string) &&
       input !== user?.email &&
-      (input as string).length > 0
+      isEmail(input) 
     ) {
       await addDoc(collection(db, "chats"), { users: [user?.email, input] });
+    } else {
+      alert("사용자 정보가 올바르지 않습니다!");
+      return;
     }
   };
   const chats = snapshot?.docs.map(
@@ -41,15 +50,19 @@ const Sidebar = () => {
   ) as Array<IChat>;
 
   const ChatList = () => {
-    return chats
-      .filter((chat) => chat.users?.includes((user as User).email as string))
-      .map((chat, idx) => {
-        return <ChatListItem key={Math.random()} chat={chat} />;
-      });
+    if (chats) {
+      return chats
+        .filter((chat) => chat.users?.includes((user as User).email as string))
+        .map((chat, idx) => {
+          return <ChatListItem key={Math.random()} chat={chat} />;
+        });
+    } else {
+      return null;
+    }
   };
 
   return (
-    <div className="flex flex-col  h-screen w-[40vw] p-4 border-r-[1px] border-borderGray">
+    <div className="flex flex-col w-[40vw]  h-screen  p-4 border-r-[1px] border-borderGray">
       <div className="flex flex-row justify-between w-full items-center">
         <div className="flex flex-row items-center">
           <Image
@@ -62,7 +75,10 @@ const Sidebar = () => {
           <span className="block ml-4 text-lg">{user?.displayName}</span>
         </div>
         <RiLogoutBoxRLine
-          onClick={() => signOut(auth)}
+          onClick={() => {
+            signOut(auth);
+            router.replace("/");
+          }}
           className="rounded-full bg-secondaryWhite"
           size={25}
         />
@@ -77,7 +93,7 @@ const Sidebar = () => {
       </div>
       {/* ChatListItem */}
       <div className="flex flex-col overflow-y-scroll scrollbar-hide">
-        {chats && ChatList()}
+        {ChatList()}
         {/* <ChatListItem /> */}
       </div>
     </div>
