8f89f839168fd9e83385f80a09948a070ab346af
nahsooyeon <nahsooyeon@gmail.com>
2022-09-27 10:44:37 +0900

    💄수정: 앱 이름

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,18 +1,26 @@
 import React, { useCallback, useRef } from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "../firebase.config";
+import { IoMdChatboxes } from "react-icons/io";
 
 const LoginForm = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <button
-        onClick={() => signInWithGoogle([""], { prompt: "select_account" })}
-        className="min-w-[250px] h-20 rounded max-w-[30vw]  bg-primary text-white"
-      >
-        SING WITH GOOGLE
-      </button>
+    <div className="w-full h-screen flex flex-col justify-center items-center ">
+      <div className="w-max  flex flex-col items-center justify-center py-8 px-4 rounded-xl bg-gradient-to-r from-[#8ad3a4] via-[#45b18f] via-3/5 to-[#45b18f]">
+        <div className=" font-sans text-white text-3xl">
+          Pomme&apos;s Chat App 🍎
+        </div>
+
+        <IoMdChatboxes size={100} color="white" />
+        <button
+          onClick={() => signInWithGoogle([""], { prompt: "select_account" })}
+          className="min-w-[250px] h-8 rounded max-w-[30vw]  bg-secondaryWhite text-secondary"
+        >
+          로그인 하기
+        </button>
+      </div>
     </div>
   );
 };
