ce01a2129eb5e790f5a890eea8a2aeb4bc1d86f6
nahsooyeon <nahsooyeon@gmail.com>
2022-09-27 14:26:50 +0900

    🔍 수정: next/Head로 메타 태그 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase.config";
 import LoginForm from "../components/Login";
 import FadeLoader from "react-spinners/FadeLoader";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, loading, error] = useAuthState(auth);
@@ -20,7 +21,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     return <LoginForm />;
   }
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Pomme&apos;s Demo Chat 🍎 </title>
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_URL}
+          key="url"
+        />
+        <meta property="og:title" content={`Pomme's Demo Chat🍎`} key="title" />
+        <meta
+          property="og:description"
+          content={"Enjoy chatting with your friend!"}
+          key="description"
+        />
+        <link rel="icon"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Pomme&apos;s Demo Chat 🍎 </title>
-        <meta name="description" content="Developed by create-next-app"></meta>
-        <link rel="icon"></link>
-      </Head>
       <div className="flex flex-row">
         <Sidebar />
       </div>
