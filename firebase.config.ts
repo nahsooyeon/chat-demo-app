378692070688387b2a4808ea25e5ffe8bfea7c0a
nahsooyeon <nahsooyeon@gmail.com>
2022-10-21 11:21:54 +0900

    🔧 수정: 환경변수 추가

--- firebase.config.ts
+++ firebase.config.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
@@ -20,5 +20,4 @@ const db = getFirestore();
 //   provider: new ReCaptchaV3Provider(process.env.RECAPTCHA),
 //   isTokenAutoRefreshEnabled: true,
 // });
-
 export { auth, db };
